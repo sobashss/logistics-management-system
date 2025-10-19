98048ada1f3f481a0ec9246df06de7d236c9a975
Saviru Sobash Jayasingha Gunawardana <as20240540@sci.sjp.ac.lk>
2025-10-19 13:45:21 +0530

    Add constants

--- logisticsmanagementsystem/main.c
+++ logisticsmanagementsystem/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_CITIES 30
+#define FUEL_PRICE 310
+
 
 int main()
 {
-    printf("Hello world!\n");
+
+
+
+
+
+
+
+
     return 0;
 }
