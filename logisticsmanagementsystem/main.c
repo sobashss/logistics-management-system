315935869f8fee99529fd329a7cd64edda1a8319
Saviru Sobash Jayasingha Gunawardana <as20240540@sci.sjp.ac.lk>
2025-10-20 11:32:23 +0530

    Add addCity function

--- logisticsmanagementsystem/main.c
+++ logisticsmanagementsystem/main.c
@@ -5,6 +5,11 @@
 #define MAX_CITIES 30
 #define FUEL_PRICE 310
 
+char cityNames[MAX_CITIES][60];
+int cityCount=0;
+
+void manageCities();
+void inputName();
 
 int main()
 {
@@ -51,17 +56,77 @@ int main()
                 printf("Invalid Choice!\n");
                 break;
 
-
         }
-
-
 
     } while (choice!=5);
 
 
-
-
-
-
     return 0;
 }
+
+void manageCities(){
+
+    int choice1=0;
+
+    do{
+        printf("1.Add a New City\n");
+        printf("2.Remove a City\n");
+        printf("3.Rename a City\n");
+        printf("4.Back to Main Menu\n");
+        printf("Enter your Choice: ");
+        scanf("%d",&choice1);
+
+        switch (choice1){
+
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default :
+                printf("Invalid Choice!\n");
+                break;
+
+       }
+
+
+    } while (choice1!=4);
+
+
+}
+
+void inputName(){
+    char newcityname[60];
+
+    if(cityCount>=MAX_CITIES){
+        printf("Error: Maximum number of cities exceeded.\n");
+    }
+
+    printf("Enter Name of the City: ");
+    fgets(newcityname,sizeof(newcityname),stdin);
+
+    int exist=0; //this means name is not exist
+
+    for(int i=0;i<cityCount;i++){
+
+        if(strcmp(cityNames[i][60],newcityname) == 1){
+            exist = 1;
+        }
+    }
+
+    if(exist=0){
+        strcpy(cityNames[cityCount],newcityname);
+        cityCount++;
+
+    }
+
+    if(exist=1){
+        printf("The city already exists.\n");
+    }
+
+}
+
+
